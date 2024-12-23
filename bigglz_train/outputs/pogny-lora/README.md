6bdb8db13a918aeaea8a99ced638e0da9175ef36
jun99 <dkssud374@gmail.com>
2024-12-23 10:42:08 +0900

    commit message

---
+++ bigglz_train/outputs/pogny-lora/README.md
@@ -0,0 +1,31 @@
+---
+tags:
+- text-to-image
+- flux
+- lora
+- diffusers
+- template:sd-lora
+- fluxgym
+
+
+base_model: black-forest-labs/FLUX.1-dev
+instance_prompt: Pogny
+license: other
+license_name: flux-1-dev-non-commercial-license
+license_link: https://huggingface.co/black-forest-labs/FLUX.1-dev/blob/main/LICENSE.md
+---
+
+# Pogny_LoRA
+
+A Flux LoRA trained on a local computer with [Fluxgym](https://github.com/cocktailpeanut/fluxgym)
+
+<Gallery />
+
+## Trigger words
+
+You should use `Pogny` to trigger the image generation.
+
+## Download model and use it with ComfyUI, AUTOMATIC1111, SD.Next, Invoke AI, Forge, etc.
+
+Weights for this model are available in Safetensors format.
+
---
+++ bigglz_train/outputs/pogny-lora/train.sh
@@ -0,0 +1,36 @@
+accelerate launch \
+  --mixed_precision bf16 \
+  --num_cpu_threads_per_process 1 \
+  sd-scripts/flux_train_network.py \
+  --pretrained_model_name_or_path "/teamspace/studios/this_studio/fluxgym/models/unet/flux1-dev.sft" \
+  --clip_l "/teamspace/studios/this_studio/fluxgym/models/clip/clip_l.safetensors" \
+  --t5xxl "/teamspace/studios/this_studio/fluxgym/models/clip/t5xxl_fp16.safetensors" \
+  --ae "/teamspace/studios/this_studio/fluxgym/models/vae/ae.sft" \
+  --cache_latents_to_disk \
+  --save_model_as safetensors \
+  --sdpa --persistent_data_loader_workers \
+  --max_data_loader_n_workers 2 \
+  --seed 42 \
+  --gradient_checkpointing \
+  --mixed_precision bf16 \
+  --save_precision bf16 \
+  --network_module networks.lora_flux \
+  --network_dim 4 \
+  --optimizer_type adamw8bit \
+  --learning_rate 8e-4 \
+  --cache_text_encoder_outputs \
+  --cache_text_encoder_outputs_to_disk \
+  --fp8_base \
+  --highvram \
+  --max_train_epochs 16 \
+  --save_every_n_epochs 4 \
+  --dataset_config "/teamspace/studios/this_studio/fluxgym/outputs/pogny-lora/dataset.toml" \
+  --output_dir "/teamspace/studios/this_studio/fluxgym/outputs/pogny-lora" \
+  --output_name pogny-lora \
+  --timestep_sampling shift \
+  --discrete_flow_shift 3.1582 \
+  --model_prediction_type raw \
+  --guidance_scale 1 \
+  --loss_type l2 \
+  --log_with tensorboard \
+  --logging_dir /teamspace/studios/this_studio/fluxgym/Tensorboard
